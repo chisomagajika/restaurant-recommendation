ef1b5862c377a288771b403d0fa39931877fc2af
TPL1 <tpl1@Techtonicas-MacBook-Air-3.local>
2019-02-06 13:22:19 -0800

    starting the project

---
+++ restaurantRecommender.js
@@ -0,0 +1,52 @@
+class RestaurantRecommender() {
+    constructor(ratings , restaurants, users) {
+    // All main properties should go here.
+    this.ratings = {};
+    this.restaurants = {};
+    this.users = {};
+    }
+    addRestaurant() {
+    
+    this.name = {};
+    this.typeOfFood = {};
+    
+
+    }
+
+    class addUser() {
+    let User = function (){
+        c
+    }
+    // Adds a new User to the System
+    }
+
+    addCategory() {
+    // Adds a new Category to the System
+    }
+
+    addRating(){
+    // Adds a user rating for a restaurant
+    }
+
+    addRestauranttoCategory(){
+    // Adds Restaurant to Category
+    // Adds Category to Restaurant
+    }
+
+
+    deleteUser() {
+    // Deletes a User from the system
+    // Deletes User ratings for a restaurant.
+    }
+
+    deleteRestaurant() {
+    // make sure to also delete a restaurant from the categories.
+    }
+
+    deleteCategory() {
+    // make sure to delete categories from a restaurant that had them.
+    }
+
+    filter(){
+    }
+}
